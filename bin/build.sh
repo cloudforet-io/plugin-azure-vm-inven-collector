d4e6280a55ff82a450746802274922abb04b4f81
khl6235 <khl6235@naver.com>
2020-09-14 17:33:23 +0900

    initial commit

---
+++ bin/build.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# Build a docker image
+cd ..
+
+#docker build -t pyengine/azure-vm . --no-cache
+#docker tag pyengine/azure-vm pyengine/azure-vm:1.0
+#docker tag pyengine/azure-vm spaceone/azure-vm:1.0
+

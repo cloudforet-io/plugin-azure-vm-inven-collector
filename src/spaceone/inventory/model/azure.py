a7fda1d58f2c3aa508c9610092672f41727c169e
Jihyung Song <bluese@megazone.com>
2020-09-15 12:04:54 +0900

    initial model

---
+++ src/spaceone/inventory/model/azure.py
@@ -0,0 +1,12 @@
+from schematics import Model
+from schematics.types import ModelType, StringType, BooleanType, ListType
+
+
+class AWSIAMInstanceProfile(Model):
+    # TODO
+    pass
+
+class Azure(Model):
+    # TODO
+    pass
+
---
+++ src/spaceone/inventory/model/server.py
@@ -0,0 +1,42 @@
+from schematics import Model
+from schematics.types import serializable, ModelType, ListType, StringType
+from spaceone.inventory.model import OS, Azure, Hardware, SecurityGroup, Compute, LoadBalancer, VNet, Subnet, \
+    NIC, Disk, ServerMetadata, Monitor
+
+
+class ReferenceModel(Model):
+    class Option:
+        serialize_when_none = False
+
+    resource_id = StringType(required=False, serialize_when_none=False)
+    external_link = StringType(required=False, serialize_when_none=False)
+
+
+class ServerData(Model):
+    os = ModelType(OS)
+    azure = ModelType(Azure)
+    hardware = ModelType(Hardware)
+    security_group = ListType(ModelType(SecurityGroup))
+    compute = ModelType(Compute)
+    load_balancer = ListType(ModelType(LoadBalancer))
+    vnet = ModelType(VNet)
+    subnet = ModelType(Subnet)
+    # auto_scaling_group = ModelType(AutoScalingGroup, serialize_when_none=False) # TODO : 찾아주세요
+    monitor = ModelType(Monitor)
+
+
+class Server(Model):
+    name = StringType()
+    region_code = StringType()
+    region_type = StringType(default='AZURE')
+    data = ModelType(ServerData)
+    nics = ListType(ModelType(NIC))
+    disks = ListType(ModelType(Disk))
+    primary_ip_address = StringType(default='')
+    ip_addresses = ListType(StringType())
+    server_type = StringType(default='VM')
+    os_type = StringType(choices=('LINUX', 'WINDOWS'))
+    provider = StringType(default='azure')
+    _metadata = ModelType(ServerMetadata, serialized_name='metadata')
+    reference = ModelType(ReferenceModel)
+
